3102c4cb7dcdf305be9b9577e69b7a5de2fb6f80
Grisha <odnokonnygrisha@gmail.com>
2023-06-30 03:18:02 +0300

    feat: task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+int[] masiv = new int[8];
+int count = 0;
+for(int i = 0; i < masiv.Length; i++)
+{
+    masiv[i] = new Random().Next(100, 1000);
+    Console.Write(masiv[i] + " ");
+    if (masiv[i] % 2 == 0)
+    {
+        count ++;
+    }
+}
+Console.WriteLine();
+Console.Write("Количество четных чисел в массиве - " + count);
